ce933d15cf770c1b06758a273c2ed1b62b7d216c
abdulaziz055 <abdulazizabdurahimov519>
2025-05-23 10:03:47 +0500

    ``` style: Update styling for image display and interaction on the home page ```

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -48,7 +48,7 @@ function Home() {
             return (
               <div
                 key={value.id}
-                className="group ml-5 cursor-pointer relative"
+                className="group ml-5 mb-5 cursor-pointer relative"
               >
                 <div className="relative rounded-md">
                   <img src={value.img} alt="" className="rounded-md" />
@@ -61,10 +61,16 @@ function Home() {
                     deleted
                   </button> */}
                 </div>
-                <div className="opacity-0 group-hover:opacity-100 bg-black/5 inset-0 absolute rounded-md">
-                  <button className="w-16 h-11 rounded-2xl bg-[#ef1751] hover:bg-[#BD001C] text-white absolute right-4 top-4">
+                <div className="opacity-0 group-hover:opacity-100 bg-black/30 inset-0 absolute rounded-md">
+                  <button className="w-16 cursor-pointer h-11 rounded-2xl bg-[#ef1751] hover:bg-[#BD001C] text-white absolute right-4 top-4">
                     Save
                   </button>
+                  <div className="w-9 h-9 flex items-center justify-center absolute bottom-4 right-4 bg-white rounded-xl">
+                    <p className="font-bold -mt-1">...</p>
+                  </div>
+                  <div className="h-9 absolute flex items-center justify-center bottom-4 right-16 bg-white rounded-xl w-9">
+                    <i className="fa-solid fa-download"></i>
+                  </div>
                 </div>
               </div>
             );
